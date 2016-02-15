99ecf554908a68fa325bc3e516395c71c188dec7
amm <amm@amm-VirtualBox>
2016-02-15 11:13:42 +0100

    dajeee dbbb

--- consegnaprogetto/amm15_pirasDavide.sql
+++ consegnaprogetto/amm15_pirasDavide.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.13.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generato il: Feb 15, 2016 alle 00:56
--- Versione del server: 5.5.41-0ubuntu0.14.04.1
--- Versione PHP: 5.5.9-1ubuntu4.6
+-- Creato il: Feb 15, 2016 alle 11:10
+-- Versione del server: 5.6.27-0ubuntu1
+-- Versione PHP: 5.6.11-1ubuntu3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `negozio`
+-- Database: `amm15_pirasDavide`
 --
 
 -- --------------------------------------------------------
@@ -27,17 +27,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `autori` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `autori`
 --
 
 INSERT INTO `autori` (`id`, `nome`) VALUES
-(10, 'Cuncordu Sas Enas'),
+(1, 'Cuncordu Sas Enas'),
 (2, 'Su Rosariu'),
 (5, 'Cuncordu de Orosei'),
 (6, 'Curcordu de Bosa'),
@@ -50,15 +49,11 @@ INSERT INTO `autori` (`id`, `nome`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `brani` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `titolo` varchar(100) NOT NULL,
   `autore` varchar(100) NOT NULL,
-  `id_autore` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_autore` (`id_autore`),
-  KEY `id_autore_3` (`id_autore`),
-  KEY `id_autore_2` (`id_autore`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
+  `id_autore` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `brani`
@@ -78,6 +73,36 @@ INSERT INTO `brani` (`id`, `titolo`, `autore`, `id_autore`) VALUES
 (29, 'Una notte tempestosa', 'Cuncordu Sas Enas', 1),
 (30, 'Sa bella anima mia', 'Cuncordu Seneghesu', 9);
 
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `autori`
+--
+ALTER TABLE `autori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `brani`
+--
+ALTER TABLE `brani`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `autori`
+--
+ALTER TABLE `autori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT per la tabella `brani`
+--
+ALTER TABLE `brani`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
